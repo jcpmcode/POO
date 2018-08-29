a93f3d32c74260902c262ff7d2400cc271f877df
Jose Carlos <jcpmcode@gmail.com>
2018-08-29 17:16:34 -0500

    Se agregó el flush

--- prog6/Banco.java
+++ prog6/Banco.java
@@ -5,6 +5,8 @@ public class Banco{
     Scanner entrada = new Scanner (System.in);
     float compra;
 
+    System.out.print("\033[H\033[2J");
+    System.out.flush();  
     System.out.println("Bienvenido, su saldo inicial es de: $2000" );
     System.out.print("Ingrese el monto de la compra que va a realizar: ");
     compra = entrada.nextFloat();

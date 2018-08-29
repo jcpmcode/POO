44269766cf94fbc566ce0ae88dfdeb874568c469
Jose Carlos <jcpmcode@gmail.com>
2018-08-29 17:19:32 -0500

    metodo para el flush

--- prog6/Banco.java
+++ prog6/Banco.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Banco{
   public static void main(String[] args) {
+    OperacionesBanco ejecutaOperac = new OperacionesBanco(2000);
     Scanner entrada = new Scanner (System.in);
     float compra;
 
-    System.out.print("\033[H\033[2J");
-    System.out.flush();  
+    ejecutaOperac.limpiarPantalla();
     System.out.println("Bienvenido, su saldo inicial es de: $2000" );
     System.out.print("Ingrese el monto de la compra que va a realizar: ");
     compra = entrada.nextFloat();
-    OperacionesBanco ejecutaOperac = new OperacionesBanco(2000);
     System.out.println("Tu saldo final es de: " + ejecutaOperac.RealizarCompra(compra));
   }
 }
--- prog6/OperacionesBanco.java
+++ prog6/OperacionesBanco.java
@@ -6,9 +6,15 @@ private float b;
   public OperacionesBanco(float saldoinicial){
     a = saldoinicial;
   }
+
   public float RealizarCompra(float compra){
     float saldo = a-compra;
 
     return saldo;
+  }
+
+  public void limpiarPantalla(){
+    System.out.print("\033[H\033[2J");
+    System.out.flush();  
   }
 }

cd12d21d1c10f8070a3759d2469d4cbea3c8cd9b
Jose Carlos <jcpmcode@gmail.com>
2018-09-19 18:50:39 -0500

    Version Final funcionando

--- prog15/OpMatrices.java
+++ prog15/OpMatrices.java
@@ -3,48 +3,94 @@ import java.util.Scanner;
 public class OpMatrices{
   Scanner entrada = new Scanner (System.in);
 
-  public void LlenarArreglo(int arreglo1[][], int tam){
-    for (int i = 0; i < 2; i++){
-      System.out.println("Matriz " + (i+1) + ": ");
-      for (int j = 0; j < tam; j++){
+  public void LlenarArreglo(int arreglo1[][], int arreglo2[][]){
+    int i, j;
+    System.out.println("Matriz 1: ");
+    for (i = 0; i < arreglo1.length; i++){
+      for (j = 0; j < arreglo1[i].length; j++){
         System.out.print("[" + i + "]" + "[" + j + "]: ");
         arreglo1[i][j] = entrada.nextInt();
       }
     }
-  }
-
-  public void ImprimirArreglo(int arreglo1[][]){
-    for (int i = 0; i < arreglo1.length - 1; i++){
-      for (int j = 0; j < arreglo1[i].length; j++){
-        System.out.println(arreglo1[i][j] + " " + arreglo1[i+1][j]);
+    System.out.println("Matriz 2: ");
+    for (i = 0; i < arreglo2.length; i++){
+      for (j = 0; j < arreglo2[i].length; j++){
+        System.out.print("[" + i + "]" + "[" + j + "]: ");
+        arreglo2[i][j] = entrada.nextInt();
       }
     }
   }
 
-  public void Sumar(int arreglo1[][]){
+  public void ImprimirArreglo(int arreglo1[][], int arreglo2[][]){
+    int i, j;
+    System.out.println("Matriz 1: ");
+    for (i = 0; i < arreglo1.length; i++){
+      for (j = 0; j < arreglo1[i].length; j++){
+        System.out.print("[" + i + "]" + "[" + j + "]: " + arreglo1[i][j]);
+      }
+    }
+    System.out.println("Matriz 2: ");
+    for (i = 0; i < arreglo2.length; i++){
+      for (j = 0; j < arreglo1[i].length; j++){
+        System.out.print("[" + i + "]" + "[" + j + "]: " + arreglo2[i][j]);
+      }
+    }
+  }
+
+  public void Sumar(int arreglo1[][], int arreglo2[][], int arreglo3[][]){
+    int i, j;
+
+    for (i = 0; i < arreglo1.length; i++){
+      for (j = 0; j < arreglo1[i].length; j++){
+        arreglo3[i][j] = arreglo1[i][j] + arreglo2[i][j];
+      }
+    }
     System.out.println("Suma:");
-    for (int i = 0; i < arreglo1.length - 1; i++){
-      for (int j = 0; j < arreglo1[i].length; j++){
-        System.out.println(arreglo1[i][j] + arreglo1[i+1][j]);
+    for (i = 0; i < arreglo3.length; i++){
+      for (j = 0; j < arreglo3[i].length; j++){
+        System.out.print(arreglo3[i][j] + " ");
       }
+      System.out.println("");
     }
   }
 
-  public void Restar(int arreglo1[][]){
-    System.out.println("Resta:");
-    for (int i = 0; i < arreglo1.length - 1; i++){
-      for (int j = 0; j < arreglo1[i].length; j++){
-        System.out.println(arreglo1[i][j] - arreglo1[i+1][j]);
+  public void Restar(int arreglo1[][], int arreglo2[][], int arreglo3[][]){
+    int i, j;
+    for (i = 0; i < arreglo1.length; i++){
+      for (j = 0; j < arreglo1[i].length; j++){
+        arreglo3[i][j] = arreglo1[i][j] - arreglo2[i][j];
       }
+    }
+    System.out.println("Resta: ");
+    for (i = 0; i < arreglo3.length; i++){
+      for (j = 0; j < arreglo3[i].length; j++){
+        System.out.print(arreglo3[i][j] + " ");
+      }
+      System.out.println("");
     }
   }
 
-  public void Multiplicar(int arreglo1[][]){
-    System.out.println("Multiplicacion:");
-    for (int i = 0; i < arreglo1.length - 1; i++){
-      for (int j = 0; j < arreglo1[i].length; j++){
-        System.out.println(arreglo1[i][j] * arreglo1[i+1][j]);
+  public void Multiplicar(int arreglo1[][], int arreglo2[][], int arreglo3[][], int r1, int r2, int c1, int c2){
+    System.out.println("Multiplicacion: ");
+    for(int i=0;i<c1;i++)
+    {
+      for(int j=0;j<r2;j++)
+      {
+        int res=0;
+        for(int h=0;h<c2;h++)
+        {
+          res=res+(arreglo1[i][h] * arreglo2[h][j]);
+        }
+        arreglo3[i][j]=res;
       }
+    }
+    for(int i=0;i<r1;i++)
+    {
+      for(int j=0;j<c2;j++)
+      {
+        System.out.print(+ arreglo3[i][j] + " ");
+      }
+      System.out.println("");
     }
   }
 
--- prog15/prog15.java
+++ prog15/prog15.java
@@ -9,16 +9,40 @@ public class prog15{
     System.out.print("\033[H\033[2J");
     System.out.flush();
     //
-    System.out.println("Ingrese el numero de elementos de cada matriz: ");
-    int tam = entrada.nextInt();
-    int arreglo1[][] = new int[2][tam];
+    System.out.println("Ingrese cuantos renglones de la matriz 1: ");
+    int r1 = entrada.nextInt();
+    System.out.println("Ingrese cuantas columnas de la matriz 1: ");
+    int c1 = entrada.nextInt();
+    System.out.println("Ingrese cuantos renglones de la matriz 2: ");
+    int r2 = entrada.nextInt();
+    System.out.println("Ingrese cuantas columnas de la matriz 2: ");
+    int c2 = entrada.nextInt();
+    int arreglo1[][] = new int[r1][c1];
+    int arreglo2[][] = new int[r2][c2];
+    int arreglo3[][] = new int[r1][c2];
 
-    ejecutaOperac.LlenarArreglo(arreglo1, tam);
-    System.out.println();
-    // ejecutaOperac.ImprimirArreglo(arreglo1);
     System.out.println("\nResultados:");
-    ejecutaOperac.Sumar(arreglo1);
-    ejecutaOperac.Restar(arreglo1);
-    ejecutaOperac.Multiplicar(arreglo1);
+    if (!(r1 == r2 && c1 == c2)){
+      System.out.println("No se puede realizar suma ni resta de las matrices por las dimensiones.");
+      if (!(c1 == r2)){
+        System.out.println("No se puede realizar multiplicacion de matrices con estas dimensiones.");
+      }
+      else {
+        ejecutaOperac.LlenarArreglo(arreglo1, arreglo2);
+        ejecutaOperac.Multiplicar(arreglo1, arreglo2, arreglo3, r1, r2, c1, c2);
+      }
+    }
+    else{
+      ejecutaOperac.LlenarArreglo(arreglo1, arreglo2);
+      ejecutaOperac.Sumar(arreglo1, arreglo2, arreglo3);
+      ejecutaOperac.Restar(arreglo1, arreglo2, arreglo3);
+      if (!(c1 == r2)){
+        System.out.println("No se puede realizar multiplicacion de matrices con estas dimensiones.");
+      }
+      else{
+        ejecutaOperac.Multiplicar(arreglo1, arreglo2, arreglo3, r1, r2, c1, c2);
+      }
+    }
+    // ejecutaOperac.ImprimirArreglo(arreglo1, arreglo2, arreglo3);
   }
 }
